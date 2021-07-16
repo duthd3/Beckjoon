4e1a81fe8a813c811842a187364ffd5d8117cd33
YeoSong <duthd3@naver.com>
2021-07-17 07:04:42 +0900

    Create 2845.java

---
+++ Bronze/Bronze5/2845.java
@@ -0,0 +1,28 @@
+package Bronze5;
+
+import java.util.Scanner;
+
+public class Bronze5_2845 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int result = a*b;
+		
+		int num1=sc.nextInt();
+		int num2=sc.nextInt();
+		int num3=sc.nextInt();
+		int num4=sc.nextInt();
+		int num5=sc.nextInt();
+		
+		System.out.print(num1-result+ " ");
+		System.out.print(num2-result + " ");
+		System.out.print(num3-result + " ");
+		System.out.print(num4-result + " ");
+		System.out.print(num5-result + " ");
+		
+	}
+
+}
