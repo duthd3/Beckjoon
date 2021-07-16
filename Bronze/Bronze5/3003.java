78ccb738ca5e1e45ddc3a7cf3bfa02bd399ca2a9
YeoSong <duthd3@naver.com>
2021-07-17 07:05:19 +0900

    Create 3003.java

---
+++ Bronze/Bronze5/3003.java
@@ -0,0 +1,24 @@
+package Bronze5;
+
+import java.util.Scanner;
+
+public class Bronze5_3003 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in); //1 1 2 2 2 8
+		int k = sc.nextInt();
+		int q = sc.nextInt();
+		int l = sc.nextInt();
+		int b = sc.nextInt();
+		int n = sc.nextInt();
+		int p = sc.nextInt();
+		System.out.print(1-k + " ");
+		System.out.print(1-q + " ");
+		System.out.print(2-l + " ");
+		System.out.print(2-b + " ");
+		System.out.print(2-n + " ");
+		System.out.print(8-p);
+	}
+
+}
