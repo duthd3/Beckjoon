1b1b9075e29b9b57cc02d22595d25acc84fe8b09
YeoSong <duthd3@naver.com>
2021-07-17 07:05:41 +0900

    Create 3046.java

---
+++ Bronze/Bronze5/3046.java
@@ -0,0 +1,15 @@
+package Bronze5;
+
+import java.util.Scanner;
+
+public class Bronze5_3046 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int m = sc.nextInt();
+		System.out.println(2*m-a);
+	}
+
+}
