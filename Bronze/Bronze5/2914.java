ceb871809ae5cf8608d0aa8c910588f029bf0a3c
YeoSong <duthd3@naver.com>
2021-07-17 07:05:09 +0900

    Create 2914.java

---
+++ Bronze/Bronze5/2914.java
@@ -0,0 +1,16 @@
+package Bronze5;
+
+import java.util.Scanner;
+
+public class Bronze5_2914 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a*(b-1)+1);
+	}
+
+}
