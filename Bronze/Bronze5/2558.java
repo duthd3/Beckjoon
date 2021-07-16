9b532d735310af5e18625b7cac4830a472e0b5ff
YeoSong <duthd3@naver.com>
2021-07-17 07:04:33 +0900

    Create 2558.java

---
+++ Bronze/Bronze5/2558.java
@@ -0,0 +1,18 @@
+package Bronze5;
+
+import java.util.Scanner;
+
+public class Bronze5_2558 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a+b);
+		
+		
+	}
+
+}
