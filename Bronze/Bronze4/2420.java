036cc4099e714f3fe428bb0697c2e634c107a125
YeoSong <duthd3@naver.com>
2021-07-18 23:26:34 +0900

    Create 2420.java

---
+++ Bronze/Bronze4/2420.java
@@ -0,0 +1,18 @@
+package Bronze4;
+
+import java.util.Scanner;
+
+public class Bronze4_2420 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextLong();
+		long b = sc.nextLong();
+		
+		long c = a-b;
+		long d = Math.abs(c);
+		System.out.println(d);
+	}
+
+}
