5f16803835465a41c10ed6ae736c0b62c7666244
YeoSong <duthd3@naver.com>
2021-07-18 23:19:36 +0900

    Update 10757.java

--- Bronze/Bronze5/10757.java
+++ Bronze/Bronze5/10757.java
@@ -1,1 +1,20 @@
 
+package Bronze5;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Bronze5_10757 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		BigInteger a = sc.nextBigInteger();
+		BigInteger b = sc.nextBigInteger();
+		
+		System.out.println(a.add(b));
+		
+		
+	}
+
+}
