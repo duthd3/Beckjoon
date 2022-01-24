210b1024a71fb469fa9d6aed828371adb68c555e
YeoSong <duthd3@naver.com>
2022-01-24 16:40:57 +0900

    Create 2751.java

---
+++ Silver/Silver5/2751.java
@@ -0,0 +1,27 @@
+package Level12;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Level12_2751 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		int N = sc.nextInt();
+		ArrayList<Integer> list = new ArrayList<>();
+		for(int i=0; i<N; i++) {
+			list.add(sc.nextInt());
+		}
+		
+		Collections.sort(list);
+		
+		for(int value : list) {
+			sb.append(value).append('\n');
+		}
+		System.out.println(sb);
+	}
+}
